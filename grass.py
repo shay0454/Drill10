6c230178e8351dfb7908495bab28398f7bf437fd
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-19 10:50:35 +0900

    클래스 생성자, draw함수 수정

--- grass.py
+++ grass.py
@@ -2,11 +2,12 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
+    def __init__(self,x=400,y=30):
+        self.x,self.y=x,y
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(self.x, self.y)
 
     def update(self):
         pass
